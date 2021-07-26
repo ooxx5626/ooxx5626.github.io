62a2c778e5dd725f329de1fb01c1b13292c5b678
ooxx5626 <lin@DESKTOP-CRS9E4M.localdomain>
2021-07-26 23:52:34 +0800

    2021-07-26

--- js/list.js
+++ js/list.js
@@ -1,8 +1,8 @@
 [{
-    name: "sami",
-    url:"https://www.youtube.com/playlist?list=PLeW9adO3j2Ehw4JPj2DhxOpBybWBs1fPh"
+    "name": "sami",
+    "url":"https://www.youtube.com/playlist?list=PLeW9adO3j2Ehw4JPj2DhxOpBybWBs1fPh"
 },{
-    name: "no_party",
-    name: "https://www.youtube.com/playlist?list=OLAK5uy_nOXUalzwOni3tKfG6gurzhvS_u3IaXYi0"
+    "name": "no_party",
+    "url": "https://www.youtube.com/playlist?list=OLAK5uy_nOXUalzwOni3tKfG6gurzhvS_u3IaXYi0"
 }
 ]
